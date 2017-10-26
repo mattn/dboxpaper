874e30a26f3d7583e841ae1bf0688a2b6a03e630
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-10-26 13:26:48 +0900

    list -json

--- cmd_list.go
+++ cmd_list.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/urfave/cli"
@@ -18,44 +19,76 @@ func init() {
 		Action: func(c *cli.Context) error {
 			dboxpaper := app.Metadata["dboxpaper"].(*DboxPaper)
 			docIds, err := listDocs(c)
-			if !c.Bool("l") {
+			if c.Bool("title") {
+				for _, item := range docIds {
+					var in, out bytes.Buffer
+					err = json.NewEncoder(&in).Encode(map[string]interface{}{"doc_id": item})
+					if err != nil {
+						return err
+					}
+					err = dboxpaper.doAPI(
+						context.Background(),
+						http.MethodPost,
+						"/2/paper/docs/get_metadata",
+						&request{
+							ct:  "application/json",
+							in:  &in,
+							out: &out,
+						})
+					if err != nil {
+						continue
+					}
+					var docmeta DocsMeta
+					err = json.NewDecoder(&out).Decode(&docmeta)
+					if err != nil {
+						return err
+					}
+					fmt.Fprintf(c.App.Writer, "%s %s\n", docmeta.DocID, docmeta.Title)
+				}
+			} else if c.Bool("json") {
+				fmt.Fprint(c.App.Writer, "[")
+				n := 0
+				for _, item := range docIds {
+					var in, out bytes.Buffer
+					err = json.NewEncoder(&in).Encode(map[string]interface{}{"doc_id": item})
+					if err != nil {
+						return err
+					}
+					if dboxpaper.doAPI(
+						context.Background(),
+						http.MethodPost,
+						"/2/paper/docs/get_metadata",
+						&request{
+							ct:  "application/json",
+							in:  &in,
+							out: &out,
+						}) != nil {
+						continue
+					}
+					if n > 0 {
+						fmt.Fprint(c.App.Writer, ",")
+					}
+					n++
+					io.Copy(c.App.Writer, &out)
+				}
+				fmt.Fprint(c.App.Writer, "]")
+			} else {
 				for _, item := range docIds {
 					fmt.Fprintln(c.App.Writer, item)
 				}
 				return nil
-			}
-			for _, item := range docIds {
-				var in, out bytes.Buffer
-				err = json.NewEncoder(&in).Encode(map[string]interface{}{"doc_id": item})
-				if err != nil {
-					return err
-				}
-				err = dboxpaper.doAPI(
-					context.Background(),
-					http.MethodPost,
-					"/2/paper/docs/get_metadata",
-					&request{
-						ct:  "application/json",
-						in:  &in,
-						out: &out,
-					})
-				if err != nil {
-					continue
-				}
-				var docmeta DocsMeta
-				err = json.NewDecoder(&out).Decode(&docmeta)
-				if err != nil {
-					return err
-				}
-				fmt.Fprintf(c.App.Writer, "%s %s\n", docmeta.DocID, docmeta.Title)
 			}
 			return nil
 		},
 	}
 	command.Flags = []cli.Flag{
 		cli.BoolFlag{
-			Name:  "l",
-			Usage: "list title",
+			Name:  "title",
+			Usage: "show title",
+		},
+		cli.BoolFlag{
+			Name:  "json",
+			Usage: "show as JSON",
 		},
 	}
 	app.Commands = append(app.Commands, command)
