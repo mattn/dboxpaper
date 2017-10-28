c24fb99ad17ce120ca1b9aa1543be7fee3ab25a2
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-10-28 22:53:12 +0900

    add missing file

---
+++ cmd_upload_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/urfave/cli"
+)
+
+func TestCmdUploadCreate(t *testing.T) {
+	got := testWithServer(
+		func(w http.ResponseWriter, r *http.Request) {
+			switch r.URL.Path {
+			case "/2/paper/docs/create":
+				h := r.Header.Get("Dropbox-API-Arg")
+				var v map[string]interface{}
+				err := json.NewDecoder(strings.NewReader(h)).Decode(&v)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if _, ok := v["doc_id"]; ok {
+					t.Fatal("bad request")
+				}
+				b, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if string(b) != "hello world" {
+					t.Fatal("bad request")
+				}
+				fmt.Fprintln(w, `{"doc_id":"xxx"}`)
+				return
+			}
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		},
+		func(app *cli.App) {
+			app.Metadata["stdin"] = strings.NewReader("hello world")
+			app.Run([]string{"dboxpaper", "upload"})
+		},
+	)
+	want := "xxx\n"
+	if got != want {
+		t.Fatalf("want %v but got %v", want, got)
+	}
+}
+
+func TestCmdUploadUpdate(t *testing.T) {
+	got := testWithServer(
+		func(w http.ResponseWriter, r *http.Request) {
+			switch r.URL.Path {
+			case "/2/paper/docs/get_metadata":
+				var v map[string]interface{}
+				err := json.NewDecoder(r.Body).Decode(&v)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if v["doc_id"].(string) != "xxx" {
+					t.Fatal("bad request")
+				}
+				fmt.Fprintln(w, `{"doc_id":"xxx","revision":123}`)
+			case "/2/paper/docs/update":
+				h := r.Header.Get("Dropbox-API-Arg")
+				var v map[string]interface{}
+				err := json.NewDecoder(strings.NewReader(h)).Decode(&v)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if v["doc_id"].(string) != "xxx" {
+					t.Fatal("bad request")
+				}
+				if int64(v["revision"].(float64)) != 123 {
+					t.Fatal("bad request")
+				}
+				b, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if string(b) != "good morning" {
+					t.Fatal("bad request")
+				}
+				fmt.Fprintln(w, `{"doc_id":"xxx"}`)
+				return
+			}
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		},
+		func(app *cli.App) {
+			app.Metadata["stdin"] = strings.NewReader("good morning")
+			app.Run([]string{"dboxpaper", "upload", "xxx"})
+		},
+	)
+	want := "xxx\n"
+	if got != want {
+		t.Fatalf("want %v but got %v", want, got)
+	}
+}
---
+++ util_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	"golang.org/x/oauth2"
+
+	"github.com/urfave/cli"
+)
+
+func testWithServer(h http.HandlerFunc, testFuncs ...func(*cli.App)) string {
+	ts := httptest.NewServer(h)
+	defer ts.Close()
+
+	cli.OsExiter = func(n int) {}
+
+	uri, _ := url.Parse(ts.URL)
+
+	var buf bytes.Buffer
+	app.Metadata = map[string]interface{}{
+		"dboxpaper": &DboxPaper{
+			uri:   uri,
+			token: nil,
+			config: &oauth2.Config{
+				Scopes: []string{},
+				Endpoint: oauth2.Endpoint{
+					AuthURL:  "https://www.dropbox.com/oauth2/authorize",
+					TokenURL: "https://api.dropboxapi.com/oauth2/token",
+				},
+				ClientID:     "nrb8y95k7yoeor6",
+				ClientSecret: "fhme6tzwkzw5og8",
+				RedirectURL:  "http://localhost:8989",
+			},
+		},
+	}
+	app.Writer = &buf
+
+	for _, f := range testFuncs {
+		f(app)
+	}
+
+	return buf.String()
+}
